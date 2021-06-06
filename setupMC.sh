ed266f15391555b1c528fdcfb58e57307f5bcdc7
Raezroth <47491977+Raezroth@users.noreply.github.com>
2021-06-05 20:25:58 -0700

    Update position of shortcut code again
    Different distro issues

--- setupMC.sh
+++ setupMC.sh
@@ -30,13 +30,14 @@ echo Setup 3/8
 if [ ! -f launcher.jar ]; then
 
     wget https://launcher.mojang.com/v1/objects/eabbff5ff8e21250e33670924a0c5e38f47c840b/launcher.jar
-    # sets up desktop file
+fi 
+ 
+     # sets up desktop file
     sudo mkdir /opt/minecraft
     sudo cp -r minecraft_logo.png /opt/minecraft/minecraft_logo.png
     sudo cp -r Minecraft.desktop /usr/share/applications/Minecraft.desktop  
     sudo cp -r launcher.jar /opt/minecraft/launcher.jar
     sudo chmod +x /usr/share/applications/Minecraft.desktop
-fi 
  
 # download java  
 echo Setup 4/8
